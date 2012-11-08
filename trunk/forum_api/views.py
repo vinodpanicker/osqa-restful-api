e9d41a49a7c49e6dbabdfd9332f59601237dd69b
dlugipg@gmail.com <dlugipg@gmail.com@ca86b242-4640-3547-647d-6a5346180305>
2012-11-08 23:08:37 +0000

    get_all_questions function reimplemented

--- trunk/forum_api/views.py
+++ trunk/forum_api/views.py
@@ -6,18 +6,22 @@ from django.views.decorators.http import require_http_methods
 from django.views.decorators.csrf import csrf_exempt
 
 from decorators import http_basic_auth
+from forum.models import Question
 
 @require_http_methods(["POST"])
 @csrf_exempt
 @http_basic_auth
-def get_questions(request):
+def get_all_questions(request):
+    """returns list of all questions"""
+
+    questions = Question.objects.filter_state(deleted=False).order_by('-last_activity_at')
 
     #import pdb
     #pdb.set_trace()
-
-    questions = {
-        "key1": "value1",
-        "key2": "value2"
-    }
     
-    return HttpResponse(simplejson.dumps(questions), mimetype='application/json')
+    response = {}
+
+    for q in questions:
+        response[q.id]=q.title
+  
+    return HttpResponse(simplejson.dumps(response), mimetype='application/json')
