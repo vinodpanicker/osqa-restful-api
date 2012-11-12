880b19e968af6b655639e1a2a4ff50801a67aa52
dlugipg@gmail.com <dlugipg@gmail.com@ca86b242-4640-3547-647d-6a5346180305>
2012-11-12 00:59:57 +0000

    Updated http_basic_auth decorator, added custom header decorator and get_user_details method

--- trunk/forum_api/urls.py
+++ trunk/forum_api/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls.defaults import *
 from django.utils.translation import ugettext as _
-from views import get_all_questions
+from views import *
 
 urlpatterns = patterns('',
-    url(r'^%s$' % _('forum_api/questions/'),  get_all_questions, name='get_all_questions'),
+    url(r'^%s$' % _('forum_api/user_details/'),  get_user_details, name='get_user_details'),
+    url(r'^%s$' % _('forum_api/questions/'),  get_all_questions, name='get_all_questions'),    
 )
