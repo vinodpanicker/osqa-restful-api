c4e6cf70f5475977ce3e6a8a37a66a1af178feb2
dlugipg@gmail.com <dlugipg@gmail.com@ca86b242-4640-3547-647d-6a5346180305>
2012-11-13 20:54:31 +0000

    Added check_user_exists decorator, added utils module, returned list of questions and user details

--- forum_api/views.py
+++ forum_api/views.py
@@ -4,38 +4,36 @@ from django.core.urlresolvers import reverse
 from django.utils import simplejson
 from django.views.decorators.http import require_http_methods
 from django.views.decorators.csrf import csrf_exempt
+from forum.models import Question, User
 
-from decorators import http_basic_auth, require_custom_header
-from forum.models import Question
+from decorators import *
+from utils import *
 
 
 @require_http_methods(["POST"])
 @csrf_exempt
 @http_basic_auth
 @require_custom_header("HTTP_USERNAME")
+@check_user_exists
 def get_user_details(request):
-    """returns user details"""
+    """returns requested user details"""
     
-    response = "not implemented yet"
-    return HttpResponse(simplejson.dumps(response), mimetype='application/json')
+    requested_user = []
+    
+    user_name = request.META['HTTP_USERNAME']    
+    requested_user.append(User.objects.get(username__iexact=user_name))
+    response = user_list(requested_user)    
 
+    return HttpResponse(simplejson.dumps(response), mimetype='application/json')
+   
 
 @require_http_methods(["POST"])
 @csrf_exempt
 @http_basic_auth
 def get_all_questions(request):
-    """returns list of all questions"""
+    """returns list of all questions sorted by last activity date"""
 
     questions = Question.objects.filter_state(deleted=False).order_by('-last_activity_at')
+    response = question_list(questions)
 
-    #import pdb
-    #pdb.set_trace()
-    
-    response = {}
-
-    for q in questions:
-        response[q.id] = response.get(q.id, {})
-        response[q.id]['id'] = q.id
-        response[q.id]['title'] = q.title
-  
     return HttpResponse(simplejson.dumps(response), mimetype='application/json')
