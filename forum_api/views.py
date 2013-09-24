64bac75d44a22a44adc094df24f22b6961ae952d
chenguofeng08qh@gmail.com <chenguofeng08qh@gmail.com@ca86b242-4640-3547-647d-6a5346180305>
2013-09-24 04:43:25 +0000

    add simple API for asking questions

--- forum_api/views.py
+++ forum_api/views.py
@@ -4,8 +4,8 @@ from django.core.urlresolvers import reverse
 from django.utils import simplejson
 from django.views.decorators.http import require_http_methods
 from django.views.decorators.csrf import csrf_exempt
-from forum.models import Question, User
-
+from forum.models import Question, User, Node
+from django.db import connection, transaction
 from decorators import *
 from utils import *
 
@@ -25,7 +25,7 @@ def get_user_details(request):
     response = user_list(requested_user)    
 
     return HttpResponse(simplejson.dumps(response), mimetype='application/json')
-   
+
 
 @require_http_methods(["POST"])
 @csrf_exempt
@@ -41,7 +41,7 @@ def get_all_questions(request):
 @require_http_methods(["POST"])
 @csrf_exempt
 @http_basic_auth
-@require_custom_header("KEYWORDS")
+@require_ask_questions_header("KEYWORDS")
 def search_questions(request):
     """returns questions mathching keywords"""
     keywords = request.POST['KEYWORDS']   
@@ -50,4 +50,37 @@ def search_questions(request):
     response = question_list(questions)
 
     return HttpResponse(simplejson.dumps(response), mimetype='application/json')
+
+@require_http_methods(["POST"])
+@csrf_exempt
+@http_basic_auth
+@require_ask_questions_header("TITLE")
+@require_ask_questions_header("TAGS")
+@require_ask_questions_header("AUTHORID")
+@require_ask_questions_header("BODY")
+def ask_questions(request):
+    """sk questions API"""  
+    qtitle = request.POST['TITLE']   
+    tags = request.POST['TAGS']
+    author = request.POST['AUTHORID']
+    qbody = request.POST['BODY']
+    p2 = Node(title = qtitle, tagnames = tags, author_id = author, body = qbody, node_type = "question")
+    "cannot succeed in setting node_type here and I don't know why"
+    p2.save()
+
+    " change node_type to question"
+    cursor = connection.cursor()
+    sql = "update forum_node set node_type = 'question' where 1 order by id desc limit 1;"
+    cursor.execute(sql)
+
+    questions = Question.objects.filter(title__contains=qtitle)
+    response = question_list(questions)
+
+    return HttpResponse(simplejson.dumps(response), mimetype='application/json')
+
+
+
+
+
+
 
